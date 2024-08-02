4c6f919989581e97dcde6b404a1301a0dd422e4a
yusufansar <yusufansar313@gmail.com>
2024-08-02 06:55:10 +0530

    Designed the UI of add loan view

--- LoanTracker/LoanTracker/AddLoanView.swift
+++ LoanTracker/LoanTracker/AddLoanView.swift
@@ -10,11 +10,21 @@ import SwiftUI
 struct AddLoanView: View {
     
     @Environment(\.dismiss) var dismiss
+    @State private var name = ""
+    @State private var amount = ""
+    @State private var startDate = Date()
+    @State private var dueDate = Date()
+
 
     var body: some View {
         NavigationStack {
-            VStack {
-
+            Form {
+                TextField("Name", text: $name)
+                    .textInputAutocapitalization(.sentences)
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+                DatePicker("Start Date", selection: $startDate, in: ...Date(), displayedComponents: .date)
+                DatePicker("Due Date", selection: $dueDate, in: startDate..., displayedComponents: .date)
             }
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
